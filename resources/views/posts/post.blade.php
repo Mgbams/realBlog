599f83efd5dbf325eb5fa33d12518d5177f3b0db
Mgbams <cmgbams@gmail.com>
2020-08-26 20:43:31 +0200

    creating post form

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -7,12 +7,82 @@
             <div class="card">
                 <div class="card-header">Post</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            posts
-                        </div>
-                    @endif
+                <div class="card-post_body pt-3 pb-3">
+
+                    <div class="row">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="post_title" class="col-md-4 col-form-label text-md-right">post_title</label>
+
+                                <div class="col-md-6">
+                                    <input id="post_title" type="text" class="form-control @error('post_title') is-invalid @enderror" name="post_title" value="{{ old('post_title') }}" required autocomplete="post_title" autofocus>
+
+                                    @error('post_title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="post_body" class="col-md-4 col-form-label text-md-right">Post Body</label>
+
+                                <div class="col-md-6">
+                                    <textarea id="post_body" rows="7" type="text" class="form-control @error('post_body') is-invalid @enderror" name="post_body" value="{{ old('post_body') }}" required autocomplete="post_body"></textarea>
+
+                                    @error('post_body')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="category_id" class="col-md-4 col-form-label text-md-right">Category</label>
+
+                                <div class="col-md-6">
+                                    <select id="category_id" type="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id" required autocomplete="category_id">
+                                        <option value="">Select</option>
+                                        <option value="">Technology</option>
+                                        <option value="">Entertainment</option>
+                                    </select>
+
+                                    @error('category_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="post_image" class="col-md-4 col-form-label text-md-right">Featured Image</label>
+
+                                <div class="col-md-6">
+                                    <input id="post_image" type="file" class="form-control @error('post_image') is-invalid @enderror" name="post_image" required>
+
+                                    @error('post_image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary btn-large btn-block">
+                                        Publish Post
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
 
                 </div>
             </div>
